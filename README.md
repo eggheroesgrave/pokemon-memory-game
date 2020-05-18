7575b1f86dad3f50cc8bc983ec22fae7ac0503b2
George Varghese <61243548+eggheroesgrave@users.noreply.github.com>
2020-05-19 02:28:32 +0530

    added core mechanics and working gameplay

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # pokemon-memory-game
-A simple game written in vanilla javscript where a player matches a pair of matching images until all pairs have been cleared
+A simple game written in vanilla javscript where a player matches images in pairs until all pairs have been cleared
--- app.js
+++ app.js
@@ -43,17 +43,72 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     ]
 
+    cardArray.sort(() => Math.random() - 0.5)
+
+    //function shuffle(array) {
+    // for (let i = cardArray.length - 1; i > 0; i--) {
+    //     let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+    
+    //     // swap elements array[i] and array[j]
+    //     // we use "destructuring assignment" syntax to achieve that
+    //     // you'll find more details about that syntax in later chapters
+    //     // same can be written as:
+    //     // let t = array[i]; array[i] = array[j]; array[j] = t
+    //     [cardArray[i], cardArray[j]] = [cardArray[j], Cardrray[i]];
+    // }
+     // }
+
+    const resultDisplay = document.querySelector('#result')
     const grid = document.querySelector('.grid')
+    var cardsChosen = [ ]
+    var cardsChosenId = [ ]
+    var cardsWon = [ ]
 
     function createBoard() {
         for (let i = 0; i < cardArray.length; i++) {
             var card = document.createElement('img')
             card.setAttribute('src', 'images/pikachu.png')
             card.setAttribute('data-id', i)
-            //card.addEventListener('click', flipcard)
+            card.addEventListener('click', flipCard)
             grid.appendChild(card)
         }
     }
+
+    function checkForMatch() {
+        var cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+        if ( cardsChosen[0] == cardsChosen[1] ) {
+            alert('It\'s a match!')
+            cards[optionOneId].setAttribute('src', 'images/ditto.png')
+            cards[optionTwoId].setAttribute('src', 'images/ditto.png')
+            cardsWon.push(cardsChosen)
+        }
+        else {
+            cards[optionOneId].setAttribute('src', 'images/pikachu.png')
+            cards[optionTwoId].setAttribute('src', 'images/pikachu.png')
+            alert('Nope, not the same')
+        }
+
+        cardsChosen = [ ]
+        cardsChosenId = [ ]
+        resultDisplay.textContent = cardsWon.length
+        if (cardsWon.length === cardArray.length/2) {
+            resultDisplay.textContent = 'Game Over! You Win!'
+        }
+    }
+
+    function flipCard() {
+        var cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if (cardsChosenId.length === 2) {
+            setTimeout(checkForMatch, 500)
+        }
+    }
+
+
 
     createBoard()
 })
